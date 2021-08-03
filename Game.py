a34a089c282e6aedc6535bba553bfb4c1c56c465
kty0205 <78518941+kty0205@users.noreply.github.com>
2021-08-03 22:29:28 +0900

    Add files via upload

--- Game.py
+++ Game.py
@@ -8,9 +8,9 @@ datalist = {} #몬스터, 아이템, 무기
 account = [] #닉네임, 레벨, 경험치, 골드, 소지무기, 소지아이템
 
 '''
-weapon.dat 형식 : 이름/가격/추가시간
+weapon.dat 형식 : 이름/가격/추가시간/판매여부(대장간:1, 상점:2)
 monster.dat 형식 : 이름/레벨/출현던전/경험치
-items.dat 형식 : 이름/판매여부/가격
+items.dat 형식 : 이름/판매여부/가격(상점)
 '''
 
 class Game:
@@ -104,7 +104,7 @@ class World:
 				elif move_place == '대장간':
 					print('===대장간===')
 				elif move_place == '상점':
-					print('상점')
+					print('===상점===')
 				elif move_place == '던전으로 가기':
 					mkstring(kindof_dungeon)
 					print('이동할 던전을 입력하세요')
@@ -122,6 +122,7 @@ class World:
 			print('비교적 평화로운 뒷산입니다')
 		while 1:
 			waiting_time = randrange(1, 5)
+			spawn_able = []
 			print('몬스터 찾는중...')
 			for i in monster_list:
 				name = list(i.keys())[0]
@@ -129,34 +130,36 @@ class World:
 				level = i[name][0]
 				xp = i[name][2]
 				if type in spawnpoint:
-					break
+					templist = [name, spawnpoint, level, xp]
+					spawn_able.append(templist)
 			if len(account[4]) >= 1:
-				name = list(account[4].keys())[0]
-				weapon_buff = float(account[4][name])
+				weapon_name = list(account[4].keys())[0]
+				weapon_buff = float(account[4][weapon_name])
 			else:
 				weapon_buff = 0
-			numsize = '1'+'0'*(int(level) * 2)
-			attack_num = randrange(int(numsize), int('9'*(int(level)*3)))+weapon_buff
-			attack_time = (100-int(level))/100
+			num = randrange(0, len(spawn_able)-1)
+			numsize = '1'+'0'*(int(spawn_able[num][2]) * 2)
+			attack_num = randrange(int(numsize), int('9'*(int(spawn_able[num][2])*3)))+weapon_buff
+			attack_time = (100-int(spawn_able[num][2]))/100
 			time.sleep(waiting_time)
 			print('몬스터 출현!')
-			print(name+', '+'Lv.'+level)
+			print(name+', '+'Lv.'+spawn_able[num][2])
 			print(attack_num)
 			starttime = time.time()
 			ans = input('>>>')
 			endtime = time.time()
-			if endtime - starttime <= attack_time:
+			if endtime - starttime <= attack_time and attack_num == ans:
 				print('승리!')
-				print('보상 : '+xp+'XP')
+				print('보상 : '+spawn_able[num][3]+'XP')
 				player_xp = int(account[2])
-				player_xp += int(xp)
+				player_xp += int(spawn_able[num][3])
 				account[2] = str(player_xp)
 			else:
 				print('실패!')
 			print('[C : 계속하기, E : 그만하기]')
 			ans = input('>>>')
 			if ans == 'E' or ans == 'e':
-				break
+				return 'go_to_town'
 			else:
 				continue
 
