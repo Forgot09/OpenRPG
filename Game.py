d50c3c3d2dc39805bb26aaca4fe3742fd7dc5cb4
kty0205 <78518941+kty0205@users.noreply.github.com>
2021-08-04 19:47:46 +0900

    Add files via upload

--- Game.py
+++ Game.py
@@ -7,24 +7,24 @@ from random import randrange
 datalist = {} #몬스터, 아이템, 무기
 account = [] #닉네임, 레벨, 경험치, 골드, 소지무기, 소지아이템
 
+#{'monster':[['슬라임', '1', '뒷산', '10'], ['벌레']]...
+
 '''
-weapon.dat 형식 : 이름/가격/추가시간/판매여부(대장간:1, 상점:2)
+weapon.dat 형식 : 이름/가격/추가시간/판매여부(1:대장간)/설명
 monster.dat 형식 : 이름/레벨/출현던전/경험치
-items.dat 형식 : 이름/판매여부/가격(상점)
+items.dat 형식 : 이름/가격/효과/판매여부(1:상점)/설명
 '''
 
 class Game:
 	def appdata(self, datafile, type):
 		global datalist
 		tempdata = datafile.readlines()
-		tempdict = {}
 		templist = []
 		for i in tempdata:
 			if '\n' in i:
 				i = i.rstrip('\n')
 			temp = i.split('/')
-			tempdict[temp[0]] = temp[1:]
-			templist.append(tempdict)
+			templist.append(temp)
 		datalist[type] = templist
 	
 	def run(self):
@@ -51,12 +51,47 @@ class Game:
 			account.append('1') #레벨
 			account.append('0') #경험치
 			account.append('500') #골드
+		
+		if os.path.isfile('data/invenweapon.dat'):
+			weapon_list = []
+			with open('data/invenweapon.dat', 'r') as data:
+				data.readlines()
+				for i in data:
+					if '\n' in i:
+						i = i.rstrip('\n')
+					weapon_list.append(i)
+			account.append(weapon_list) #소지무기
+		else:
+			account.append([])
+		if os.path.isfile('data/inventory.dat'):
+			inven_list = []
+			with open('data/inventory.dat', 'r') as data:
+				data.readlines()
+				for i in data:
+					if '\n' in i:
+						i = i.rstrip('\n')
+					inven_list.append(i)
+			account.append(inven_list) #소지아이템
+		else:
+			account.append([])
 	
 	def saveacc(self):
 		savedata = ''
-		for i in account:
-			savedata = savedata + i + '/'
+		for i in range(0, 3):
+			savedata = savedata + account[i] + '/'
 		with open('data/account.dat', 'w') as file:
+			file.write(savedata)
+		savedata = ''
+		invenlist = account[4]
+		for i in invenlist:
+			savedata = savedata + i + '\n'
+		with open('data/invenweapon.dat', 'w') as file:
+			file.write(savedata)
+		savedata = ''
+		invenlist = account[5]
+		for i in invenlist:
+			savedata = savedata + i + '\n'
+		with open('data/inventory.dat', 'w') as file:
 			file.write(savedata)
 
 class Server:
@@ -65,13 +100,16 @@ class Server:
 class ComputeFunc:
 	pass
 
-town_building = ['집', '대장간', '상점', '던전으로 가기']
-kindof_dungeon = ['뒷산']
-home_content = ['정보 보기', '저장', '저장 및 나가기']
+town_building = ['집', '대장간', '상점', '숲','던전으로 가기', '퀘스트']
+kindof_dungeon = ['뒷산', '동굴']
+home_content = ['정보 보기', '저장', '저장 및 종료']
+smithy_selling = []
+shop_selling = []
 
 class World:
 	g = Game()
 	def town(self):
+		global smithy_selling, shop_selling, account
 		while 1:
 			g.saveacc()
 			print('=====마을=====')
@@ -90,7 +128,7 @@ class World:
 						print('없는 활동입니다')
 					else:
 						if ans == '정보 보기':
-							print('OpenRPG - v0.02-dev')
+							print('OpenRPG - v0.10-beta')
 							print('닉네임 : '+account[0])
 							print('레벨 : '+account[1])
 							print('경험치 : '+account[2])
@@ -103,8 +141,10 @@ class World:
 							sys.exit()
 				elif move_place == '대장간':
 					print('===대장간===')
+					selling(['weapon', '1', smithy_selling, '대장장이'])
 				elif move_place == '상점':
 					print('===상점===')
+					selling(['items', '1', shop_selling, '상인'])
 				elif move_place == '던전으로 가기':
 					mkstring(kindof_dungeon)
 					print('이동할 던전을 입력하세요')
@@ -114,52 +154,68 @@ class World:
 						continue
 					else:
 						return 'go_to_dungeon:'+move_place
+				elif move_place == '퀘스트':
+					quest()
 	
 	def dungeon(self, type):
+		global account
 		monster_list= datalist['monster']
 		print('====='+type+'=====')
 		if type == '뒷산':
 			print('비교적 평화로운 뒷산입니다')
+		elif type == '동굴':
+			print('습하고 으스스한 동굴입니다')
 		while 1:
 			waiting_time = randrange(1, 5)
 			spawn_able = []
 			print('몬스터 찾는중...')
 			for i in monster_list:
-				name = list(i.keys())[0]
-				spawnpoint = i[name][1]
-				level = i[name][0]
-				xp = i[name][2]
-				if type in spawnpoint:
+				name = i[0]
+				spawnpoint = i[2]
+				level = i[1]
+				xp = i[3]
+				if type == spawnpoint:
 					templist = [name, spawnpoint, level, xp]
 					spawn_able.append(templist)
 			if len(account[4]) >= 1:
-				weapon_name = list(account[4].keys())[0]
-				weapon_buff = float(account[4][weapon_name])
+				weapon_buff = float(account[4][2])
 			else:
 				weapon_buff = 0
 			num = randrange(0, len(spawn_able)-1)
 			numsize = '1'+'0'*(int(spawn_able[num][2]) * 2)
+			if len(account[5]) >= 1:
+				if 'ADDTIME' in account[5][2]:
+					weapon_buff += float(account[5][2].split(':')[1])
 			attack_num = randrange(int(numsize), int('9'*(int(spawn_able[num][2])*3)))+weapon_buff
-			attack_time = (100-int(spawn_able[num][2]))/100
+			attack_time = (100-int(spawn_able[num][2]))/10
 			time.sleep(waiting_time)
 			print('몬스터 출현!')
-			print(name+', '+'Lv.'+spawn_able[num][2])
+			print(spawn_able[num][0]+', '+'Lv.'+spawn_able[num][2])
 			print(attack_num)
 			starttime = time.time()
 			ans = input('>>>')
 			endtime = time.time()
-			if endtime - starttime <= attack_time and attack_num == ans:
+			if endtime - starttime <= attack_time and attack_num == int(ans):
 				print('승리!')
 				print('보상 : '+spawn_able[num][3]+'XP')
 				player_xp = int(account[2])
 				player_xp += int(spawn_able[num][3])
 				account[2] = str(player_xp)
+				checkquest(spawn_able[num][0])
+				checklevel()
 			else:
 				print('실패!')
-			print('[C : 계속하기, E : 그만하기]')
+			g.saveacc()
+			print('[C : 계속하기, I : 정보 보기, E : 마을로 가기]')
 			ans = input('>>>')
 			if ans == 'E' or ans == 'e':
 				return 'go_to_town'
+			elif ans == 'I' or ans == 'i':
+				print('OpenRPG - v0.10-beta')
+				print('닉네임 : '+account[0])
+				print('레벨 : '+account[1])
+				print('경험치 : '+account[2])
+				print('골드 : '+account[3])
 			else:
 				continue
 
@@ -172,7 +228,104 @@ def mkstring(list):
 			string = string+list[i]
 	print('《'+string+'》')
 
-print('=====OpenRPG v0.02-dev=====\n옛날감성 텍스트 RPG')
+def say(string):
+	print(string)
+	time.sleep(1)
+
+def selling(list):
+	global account
+	print(account)
+	info = datalist[list[0]]
+	for i in info:
+		if i[3] == list[1]:
+			list[2].append(i[0])
+	say(list[3]+' : 뭐 살거라도 있어?')
+	mkstring(list[2])
+	say(list[3]+' : 살거를 골라봐')
+	ans = input('>>>')
+	if not ans in list[2]:
+		say(list[3]+' : 잘가')
+	else:
+		for i in info:
+			if not ans in i:
+				continue
+			else:
+				price = i[1]
+				if list[0] == 'weapon':
+					plus_time = i[2]
+				elif list[0] == 'item':
+					cmd = i[3]
+				description = i[4]
+				break
+		print('설명 : '+description)
+		say(list[3]+' : '+ans+'은(는) '+price+'골드야')
+		say(list[3]+' : 정말 살거야? [응, 아니]')
+		yon = input('>>>')
+		if yon == '응':
+			gold = int(account[3])
+			if gold >= int(price):
+				gold -= int(price)
+				account[3] = str(gold)
+				say(list[3]+' : 자, 여기 '+ans+'이야')
+				account[4] = [ans, plus_time]
+			else:
+				say(list[3]+' : 돈이 부족해')
+		else:
+			say(list[3]+' : 잘가')
+
+quest_list = ['슬라임 3마리 잡기', '벌레 10마리 잡기']
+reward_list = ['500 골드', '800 골드']
+have_quest = []
+quest_data = []
+def quest():
+	global have_quest, account
+	if len(have_quest) == 0:
+		num = randrange(0, len(quest_list))
+		have_quest.append(quest_list[num])
+		have_quest.append(reward_list[num])
+		say('마을 주민 : 퀘스트를 주겠네')
+		print('내용 : '+have_quest[0])
+		print('보상 : '+have_quest[1])
+	else:
+		quest_complete = checkquest('check')
+		if quest_complete:
+			say('마을 주민 : 보상을 주겠네')
+			if '골드' in have_quest[1]:
+				gold = int(account[3])
+				gold += int(have_quest[1].split(' ')[0])
+				account[3] = str(gold)
+			have_quest = []
+			quest_data = []
+		else:
+			say('마을 주민 :  퀘스트는 잘 하고있나?')
+			say('마을 주민 : 아직 안끝난것 같은데?')
+
+def checkquest(type):
+	global quest_data
+	if type == 'check':
+		if have_quest[0] == '슬라임 3마리 잡기':
+			if quest_data.count('슬라임') >= 3:
+				return True
+			else:
+				return False
+		elif have_quest[0] == '벌레 10마리 잡기':
+			if quest_data.count('벌레') >= 10:
+				return True
+			else:
+				return False
+	else:
+		quest_data.append(type)
+
+def checklevel():
+	global account
+	level = account[1]
+	xp = account[2]
+	next_level_limit = (int(level)+1) * 100
+	if int(xp) >= int(next_level_limit):
+		level = str(int(level) + 1)
+		account[1] = level
+
+print('=====OpenRPG v0.10-beta=====\n옛날감성 텍스트 RPG')
 while 1:
 	print('[S : 게임시작, E : 나가기]')
 	ans = input('>>>')
